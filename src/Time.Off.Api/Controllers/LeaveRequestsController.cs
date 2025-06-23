cd8f85dca90e3926162effb6df0125e7c1c60c24
Sarra BOUALLAGUI <sarra.bouallagui@veepee.com>
2025-06-23 12:59:33 +0200

    feat: add swagger documentation

--- src/Time.Off.Api/Controllers/LeaveRequestsController.cs
+++ src/Time.Off.Api/Controllers/LeaveRequestsController.cs
@@ -1,20 +1,40 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
 using Time.Off.Application.UseCases.SubmitLeaveRequest;
+
+namespace Time.Off.Api.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class LeaveRequestsController(SubmitLeaveRequestHandler handler) : ControllerBase
+public class LeaveRequestsController(SubmitLeaveRequestHandler submitHandler) : ControllerBase
 {
-    private readonly SubmitLeaveRequestHandler _handler = handler;
+    private readonly SubmitLeaveRequestHandler _submitHandler = submitHandler;
 
     [HttpPost]
-    public async Task<IActionResult> RequestLeave([FromBody] RequestLeaveCommand command)
+    [SwaggerOperation(
+        Summary = "Submit a leave request",
+        Description = "Allows an employee to submit a leave request with specified dates, leave type, and optional comments."
+    )]
+    [SwaggerResponse(200, "Leave request submitted successfully", typeof(object))]
+    [SwaggerResponse(400, "Invalid leave request data or validation error", typeof(object))]
+    public async Task<IActionResult> RequestLeave([FromBody, SwaggerParameter("The leave request details", Required = true)] RequestLeaveCommand command)
     {
-        var result = await _handler.HandleAsync(command);
+        var result = await _submitHandler.HandleAsync(command);
 
         if (!result.IsSuccess)
             return BadRequest(new { Error = result.ErrorMessage});
 
         return Ok(new { RequestId = result.Value });
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        //var result = await _getHandler.HandleAsync(id);
+
+        //if (!result.IsSuccess)
+        //    return NotFound(new { Error = result.ErrorMessage });
+
+        return Ok();
     }
 }
